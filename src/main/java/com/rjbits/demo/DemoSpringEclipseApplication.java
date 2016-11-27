c93e6cf15d19690888f3f41d7b234238f84297f5
sezzh <sezzhltd@gmail.com>
2016-11-27 10:59:59 -0600

    initial commit

---
+++ src/main/java/com/rjbits/demo/DemoSpringEclipseApplication.java
@@ -0,0 +1,12 @@
+package com.rjbits.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringEclipseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringEclipseApplication.class, args);
+    }
+}
---
+++ src/main/java/com/rjbits/demo/controller/WarController.java
@@ -0,0 +1,17 @@
+package com.rjbits.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")    
+public class WarController {
+
+    @RequestMapping(value = "/bebi", method = RequestMethod.GET)
+    public String get() {
+        return "Hola mundo :D";
+    }
+}
+
+
